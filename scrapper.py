90420c4713f8fc53a2ac3408da99a0cdd81ca85a
2571284d <matthew.degouveia@yahoo.co.uk>
2023-01-17 23:54:05 +0000

    scraper do be scraping tho

--- scrapper.py
+++ scrapper.py
@@ -1,1 +1,7 @@
-import pandas
+import pandas as pd
+
+def scrape (url):
+    table = pd.read_html(url)[0]
+    #tidy up table, remove notes section and lines between rows 
+    table = table[::2].iloc[:, :-1]
+    return table 
