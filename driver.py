7fc0ecc28539abf04913a03395bf046680ff79a9
2571284d <matthew.degouveia@yahoo.co.uk>
2023-05-11 04:36:34 +0100

    Code cleanup + GitIgnore Update

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 from scrapper import scraper
 from pythag_expct import pythag_expct
 
-table = scraper("https://fbref.com/en/comps/9/Premier-League-Stats")
-print(pythag_expct(table, 'Manchester Utd'))
+if __name__ == "__main__":
+    table = scraper("https://fbref.com/en/comps/9/Premier-League-Stats")
+    print(pythag_expct(table, 'Manchester Utd'))
--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-def scraper (url):
+
+def scraper(url):
     table = pd.read_html(url)[0]
-    #tidy up table, remove notes section and lines between rows 
+    # tidy up table, remove notes section and lines between rows
     table = table.iloc[:, :-1]
-    return table 
+    return table
